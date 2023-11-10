d7323f7193c215cec1268611fcd67f47f49a8363
CHOI, HYUNGJUN <115517831+choihyungjun1020@users.noreply.github.com>
2023-11-11 03:04:54 +0900

    [D2] Title: 가랏! RC카!, Time: 145 ms, Memory: 58,548 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,5 +1,3 @@
-# 구현문제
-
 T = int(input())
 
 for t in range(1, T + 1):
@@ -7,17 +5,13 @@ for t in range(1, T + 1):
     length = 0
     speed = 0
     for _ in range(N):
-        value = list(map(int, input().split()))
-        if value[0] == 0:
-            length += speed
-        elif value[0] == 1:
-            speed += value[1]
-            length += speed
-        else:
-            if value[1] > speed:
+        command = list(map(int, input().split()))
+        if command[0] == 1:
+            speed += command[1]
+        elif command[0] == 2:
+            if (speed - command[1]) < 0:
                 speed = 0
-                length = length
             else:
-                speed -= value[1]
-                length += speed
+                speed -= command[1]
+        length += speed
     print(f"#{t} {length}")
