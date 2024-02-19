9ae31eee3badddbff57de6157d9e612c15371de0
CHOI, HYUNGJUN <115517831+choihyungjun1020@users.noreply.github.com>
2024-02-19 17:01:39 +0900

    [level 2] Title: 주식가격, Time: 91.84 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,14 @@
+from collections import deque
+
 def solution(prices):
-    answer = []
-    
-    for i in range(len(prices)): 
-        count = 0
-        
-        for j in range(i + 1, len(prices)):
-            if(prices[i] <= prices[j]):
-                count += 1
-            else:
+    n = len(prices)
+    answer = [0] * n
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            answer[i] += 1
+            
+            if prices[i] > prices[j]:
                 break
-                
-        answer.append(count);
-        
-    answer.append(0); # 마지막은 변화량이 없으므로 0을 append (Java의 add)
+    
     return answer
