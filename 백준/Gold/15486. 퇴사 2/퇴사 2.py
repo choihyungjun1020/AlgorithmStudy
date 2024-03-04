0a0f785e6f6a45da4fff7f4335c6bbf06f6bfdd1
CHOI, HYUNGJUN <115517831+choihyungjun1020@users.noreply.github.com>
2024-03-04 19:54:22 +0900

    [Gold V] Title: 퇴사 2, Time: 2168 ms, Memory: 168732 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -2,35 +2,25 @@ import sys
 
 n = int(sys.stdin.readline())
 
-t = [] # 각 상담을 완료하는데 걸리는 기간
-p = [] # 각 상담을 완료했을 때 받을 수 있는 금액
+# 상담을 하는데 필요한 시간 리스트 t
+# 상담을 하면 얻을 수 있는 이익 리스트 p
+t, p = [], []
 
 # i번째 날부터 마지막 날까지 낼 수 있는 최대 이익
-dp = [0] * (n+1)
+dp = [0] * (n + 1)
 
 for _ in range(n):
-
     # 날짜별 상담 완료 기간 및 상담 완료 금액 저장
     x, y = map(int, sys.stdin.readline().split(" "))
     t.append(x)
     p.append(y)
 
-# i번째 날부터 마지막 날까지 낼 수 있는 최대 이익
 max_value = 0
 
-# 마지막 날짜부터 역순으로 탐색
-for i in range(n-1, -1, -1):
+for i in range(n - 1, -1, -1):
+    if i + t[i] <= n:  # 상담이 끝나는 날짜가 n을 초과하지 않는 경우
+        dp[i] = max(dp[i + t[i]] + p[i], dp[i + 1])
+    else:  # 상담을 할 수 없는 경우
+        dp[i] = dp[i + 1] # 다음 날의 이익을 현재 날짜에 반영
 
-    # 해당 날짜에 상담을 진행할 수 있다면
-    if (i + t[i] <= n):
-
-        # [현재 상담 일자의 이윤 + 현재 상담을 마친 일자부터의 최대 이윤]과
-        # 현재 날짜부터 마지막 날까지 낼 수 있는 최대 금액을 비교
-        dp[i] = max(p[i] + dp[i + t[i]], max_value)
-        max_value = dp[i]
-
-    # 해당 날짜에 상담을 진행할 수 없다면
-    else:
-        dp[i] = max_value
-
-print(max_value)
+print(dp[0])
